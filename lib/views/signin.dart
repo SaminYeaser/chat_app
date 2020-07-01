78d11820df24586eda10cd987a4a5b168d276346
Samin <saminyeaser1@gmail.com>
2020-07-01 13:03:53 +0600

    scrollview added

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -13,29 +13,103 @@ class _SignInState extends State<SignIn> {
       appBar: appBarMain(context),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              style: textFieldStyle(),
-              decoration: inputTextDecoration('Email'),
-            ),
-            TextField(
-              style: textFieldStyle(),
-              decoration: inputTextDecoration('Password'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              child: Container(
-                child: Text(
-                  'Forget Password?',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  style: textFieldStyle(),
+                  decoration: inputTextDecoration('Email'),
                 ),
-              ),
-            )
-          ],
+                TextField(
+                  style: textFieldStyle(),
+                  decoration: inputTextDecoration('Password'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    child: Text(
+                      'Forget Password?',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 18),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ]
+                    )
+                  ),
+                  child: Text('Sign in',
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white
+                  ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 18),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.white
+                  ),
+                  child: Text('Sign In with Google',
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17
+                    ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text('Register',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.white,
+                      fontSize: 17
+                    ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
