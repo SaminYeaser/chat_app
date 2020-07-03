cacab9ab376d3bb1fe356bd1f3a4aef1f5b1e7ca
Samin <saminyeaser1@gmail.com>
2020-07-03 17:39:51 +0600

    database done

--- lib/helper/database.dart
+++ lib/helper/database.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseMethods{
      getUsername(String username){
-
+      Firestore.instance.collection('users').where("name", isEqualTo: username).getDocuments();
      }
      uploadUserFunction(userMap){
        Firestore.instance.collection("users").add(userMap);
