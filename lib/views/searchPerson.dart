a5b75852cc873653c23b76cb1c9a9054b7a59e5f
Samin <saminyeaser1@gmail.com>
2020-07-03 11:40:26 +0600

    search is ready

--- lib/views/searchPerson.dart
+++ lib/views/searchPerson.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 class SearchPerson extends StatefulWidget {
   @override
@@ -7,6 +8,36 @@ class SearchPerson extends StatefulWidget {
 class _SearchPersonState extends State<SearchPerson> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+             Container(
+               color: Colors.white30,
+
+               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white
+                        ),
+                        decoration: InputDecoration(
+                          hintText:'Search Username',
+                          border: InputBorder.none,
+                        ),
+                      )
+                  ),
+                  Image.asset("assets/images/search_white.png")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
